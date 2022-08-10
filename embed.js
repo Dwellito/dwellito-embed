b85519daf4727a3e56a78b72273f816b6379accb
Christopher Hiller <chris@hiller.io>
2022-08-10 16:44:53 -0700

    Update embed.js

--- embed.js
+++ embed.js
@@ -30,7 +30,7 @@ const add_dwellito_style = () => {
   
   .darken { 
     background: rgba(0, 0, 0, 0.5); 
-    z-index: 100;
+    z-index: 100000000;
     position: absolute;
     top: 0;
     left: 0;
